cd2e0855d65033e3db52017e0a8bad63b5aeeb56
vomatsur <bahriyorovbahrom@gmail.com>
2025-05-08 16:30:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+alert('Malumotingiz togrimi')
+
+let a = prompt('write something')
+let b = prompt('write something')
+let c = prompt('write something')
+let d = prompt('write something')
+let z = prompt('write something')
+
+console.log( `${a}  \n ${b}  \n   ${c}  \n   ${d}  \n   ${z}`)
